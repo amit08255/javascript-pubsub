a160943ae7ebc1dc189e828019c9078bc4ce3803
amit08255 <amitcute3@gmail.com>
2020-12-23 00:48:48 +0530

    Fixed eslint errors. Added publish sync feature.

--- index.js
+++ index.js
@@ -1,72 +1,83 @@
-(function(scope) {
-	'use strict';
-	let pubsubInstance = null;
-    
+// eslint-disable-next-line func-names
+(function (scope) {
+    let pubsubInstance = null;
+
     function Pubsub() {
         let channels = {};
-    
+
         function getSubscribers(channel) {
             channels[channel] = channels[channel] || [];
             return channels[channel];
         }
-    
+
         function setSubscriber(channel, callback) {
             getSubscribers(channel);
             channels[channel].push(callback);
         }
-    
+
         return {
-            clearSubscribers: async function(channel = null){
-                if(channel){
-                    return channels[channel] = [];
+            async clearSubscribers(channel = null) {
+                if (channel) {
+                    channels[channel] = [];
                 }
 
                 channels = [];
             },
-            publish: async function(channel, data) {
-                let channels = getSubscribers(channel);
-    
-                if(channels.length > 0) {
-                    return channels[0](data);
+            async publish(channel, data) {
+                const channelList = getSubscribers(channel);
+
+                if (channelList.length > 0) {
+                    return channelList[0](data);
                 }
+
+                return Promise.resolve();
             },
-            publishAll: async function(channel, config, data) {
-                config = config || {
-                    promiseMethod: 'all'
+            publishSync(channel, data) {
+                const channelList = getSubscribers(channel);
+
+                if (channelList.length > 0) {
+                    return channelList[0](data);
+                }
+
+                return undefined;
+            },
+            async publishAll(channel, config, data) {
+                const configData = config || {
+                    promiseMethod: 'all',
                 };
-                let channels = getSubscribers(channel);
-    
-                if(channels.length > 0) {
-                    const promises = channels.map(function(chan){
-                        return chan(data);
-                    });
-                    return Promise[config.promiseMethod](promises);
+                const channelList = getSubscribers(channel);
+
+                if (channelList.length > 0) {
+                    const promises = channelList.map((chan) => chan(data));
+                    return Promise[configData.promiseMethod](promises);
                 }
                 return Promise.resolve();
             },
-            subscribe: async function(channel, callback) {
+            async subscribe(channel, callback) {
                 setSubscriber(channel, callback);
-            }
+            },
+        };
+    }
+
+    pubsubInstance = new Pubsub();
+
+    // if sbd's using requirejs library to load pubsub.js
+    if (typeof define === 'function') {
+        // eslint-disable-next-line no-undef
+        define(pubsubInstance);
+    }
+
+    // node.js
+    if (typeof module === 'object' && module.exports) {
+        module.exports = pubsubInstance;
+    }
+
+    // browser
+    if (typeof window === 'object') {
+        window.pubsub = pubsubInstance;
+        if (window !== scope) {
+            // eslint-disable-next-line no-param-reassign
+            scope.pubsub = pubsubInstance;
         }
     }
-
-	pubsubInstance = new Pubsub();
-
-	//if sbd's using requirejs library to load pubsub.js
-	if(typeof define === 'function') {
-		define(pubsubInstance);
-	}
-
-	//node.js
-	if(typeof module === 'object' && module.exports) {
-		module.exports = pubsubInstance;
-    }
-
-    //browser
-	if(typeof window === 'object') {
-		window.pubsub = pubsubInstance;
-		if(window !== scope) {
-			scope.pubsub = pubsubInstance;
-		}
-	}
-})(this);
+}(this));
